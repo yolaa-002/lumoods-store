45dae307dfd26baf6ea7d4c8490a9765a9c6aae3
yola yulia efendi <n1446183@gmail.com>
2025-09-04 13:57:45 +0700

    Perubahan pada routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/beranda', function () {
+Route::get('/', function () {
     return view('beranda');
 });
 
