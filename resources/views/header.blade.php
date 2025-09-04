c28fd89ddafa019e6a20c8d7fcd739c669d3cb54
yola yulia efendi <n1446183@gmail.com>
2025-09-04 13:59:13 +0700

    perubahan pada user/header.blade.php

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -8,7 +8,7 @@
                <div class="collapse navbar-collapse" id="navbarSupportedContent">
                   <ul class="navbar-nav ml-auto">
                      <li class="nav-item active">
-                        <a class="nav-link" href="beranda">Beranda</a>
+                        <a class="nav-link" href="/">Beranda</a>
                      </li>
                      {{-- <li class="nav-item">
                         <a class="nav-link" href="tentang">Tentang</a>
