4a9ab433f20aac47b6dd4557aa41569a9e1eba99
yola yulia efendi <n1446183@gmail.com>
2025-09-05 15:05:42 +0700

    melakukan perubahan pada beranda.blade.php

--- resources/views/beranda.blade.php
+++ resources/views/beranda.blade.php
@@ -221,14 +221,14 @@
     </div>
     <!-- copyright section end -->
     <!-- Javascript files-->
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.bundle.min.js"></script>
-    <script src="js/jquery-3.0.0.min.js"></script>
-    <script src="js/plugin.js"></script>
+    <script src="{{ asset("js/jquery.min.js") }}"></script>
+    <script src="{{ asset("js/popper.min.js") }}"></script>
+    <script src="{{ asset("js/bootstrap.bundle.min.js") }}"></script>
+    <script src="{{ asset("js/jquery-3.0.0.min.js") }}"></script>
+    <script src="{{ asset("js/plugin.js") }}"></script>
     <!-- sidebar -->
-    <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
-    <script src="js/custom.js"></script>
+    <script src="{{ asset("js/jquery.mCustomScrollbar.concat.min.js") }}"></script>
+    <script src="{{ asset("js/custom.js") }}"></script>
     <!-- javascript -->
 </body>
 
