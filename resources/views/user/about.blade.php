6c939534ff825fd67439c80ab99cc2b714d61656
yola yulia efendi <n1446183@gmail.com>
2025-09-06 11:15:31 +0700

    tentang

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -2,12 +2,14 @@
          <div class="container">
             <div class="row">
                <div class="col-md-6">
-                  <div class="about_img"><img src="images/about-img.png"></div>
+                  <div class="about_img"><img src="{{ asset("images/windi.png") }}"></div>
                </div>
                <div class="col-md-6">
-                  <h1 class="about_taital">About Icecream</h1>
-                  <p class="about_text">Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore euconsectetur adipiscing esequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu</p>
-                  <div class="read_bt_1"><a href="#">Read More</a></div>
+                  <h1 class="about_taital">Tentang Lumoods</h1>
+                  <p class="about_text">ES LUMOODS adalah minuman jelly revolusioner yang tidak hanya menyegarkan dahaga, tapi juga mengangkat mood Anda! Dengan tekstur jelly yang unik dan beragam rasa yang menggugah selera, setiap tegukan ES LUMOODS adalah pengalaman yang membahagiakan.
+                     hadir untuk mengubah mood kamu dari yang biasa-biasa saja menjadi luar biasa! Setiap rasa dirancang khusus untuk memberikan sensasi yang berbeda dan meningkatkan semangat hari-hari kamu.
+                  </p>
+                  <div class="read_bt_1"><a href="#">Selengkapnya</a></div>
                </div>
             </div>
          </div>
