95323697fa89bad8b4c7696d8639e0069e82137a
yola yulia efendi <n1446183@gmail.com>
2025-09-06 10:43:28 +0700

    hanya menubah faray

--- resources/views/user/dashboard.blade.php
+++ resources/views/user/dashboard.blade.php
@@ -1,47 +1,22 @@
 <div class="banner_section layout_padding">
             <div class="container">
                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-                  <ol class="carousel-indicators">
+                  {{-- <ol class="carousel-indicators">
                      <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active">01</li>
                      <li data-target="#carouselExampleIndicators" data-slide-to="1">02</li>
                      <li data-target="#carouselExampleIndicators" data-slide-to="2">03</li>
                      <li data-target="#carouselExampleIndicators" data-slide-to="3">04</li>
-                  </ol>
+                  </ol> --}}
                   <div class="carousel-inner">
                      <div class="carousel-item active">
                         <div class="row">
                            <div class="col-sm-6">
                               <h1 class="banner_taital">Mau yg cepet naikin moods?</h1>
-                              <p class="banner_text">Ya Lumoods, minuman jelly tinggi serat dan kalsium.Bikin moods kamu jadi bagus!</p>
+                              <p class="banner_text">Ya Lumoods, minuman jelly tinggi serat dan kalsium.Bikin moods kamu jadi bagus</p>
                               <div class="started_text"><a href="#">Beli sekarang</a></div>
                            </div>
                            <div class="col-sm-6">
                               <div class="banner_img"><img src="images/faray.png"></div>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="carousel-item">
-                        <div class="row">
-                           <div class="col-sm-6">
-                              <h1 class="banner_taital">Capek? Haus? kuyy</h1>
-                              <p class="banner_text">Guys, jangan ngaku anak hits kalo belum nyobain Es Lumoods – Es Jelly Susu Sensasi segar + jelly kenyal bikin lidah auto joget. Buruan cobain, jangan sampai kehabisan!</p>
-                              <div class="started_text"><a href="#">Beli sekarang</a></div>
-                           </div>
-                           <div class="col-sm-6">
-                              <div class="banner_img"><img src="images/windi.png"></div>
-                           </div>
-                        </div>
-                     </div>
-                     <div class="carousel-item">
-                        <div class="row">
-                           <div class="col-sm-6">
-                              <h1 class="banner_taital">Lumoods bikin moods</h1>
-                              <p class="banner_text">Rasain kenikmatan susu creamy berpadu dengan jelly kenyal yang segar… itulah Es Lumoods 💙.
- <!-- #region -->                                     Bukan cuma minuman, tapi penambah mood di setiap tegukan!</p>
-                              <div class="started_text"><a href="#">Beli sekarang</a></div>
-                           </div>
-                           <div class="col-sm-6">
-                              <div class="banner_img"><img src="images/hafiz.png"></div>
                            </div>
                         </div>
                      </div>
