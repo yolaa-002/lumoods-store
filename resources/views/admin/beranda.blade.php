a89e7c4554a3db937c15d8cbfe176f3b406d7599
yola yulia efendi <n1446183@gmail.com>
2025-09-09 08:11:52 +0700

    fitur admin

--- resources/views/admin/beranda.blade.php
+++ resources/views/admin/beranda.blade.php
@@ -8,7 +8,7 @@
         <meta name="author" content="" />
         <title>Dashboard - Admin</title>
         <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-        <link href="{{ asset("css_admin/styles.css") }}" rel="stylesheet" />
+        <link href="{{ asset("public/css_admin/styles.css") }}" rel="stylesheet" />
         <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
     </head>
     <body class="sb-nav-fixed">
@@ -677,11 +677,11 @@
             </div>
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset("js_admin/scripts.js") }}"></script>
+        <script src="{{ asset("public/js_admin/scripts.js") }}"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset("assets_admin/demo/chart-area-demo.js") }}"></script>
-        <script src="{{ asset("assets_admin/demo/chart-bar-demo.js") }}"></script>
+        <script src="{{ asset("public/assets_admin/demo/chart-area-demo.js") }}"></script>
+        <script src="{{ asset("public/assets_admin/demo/chart-bar-demo.js") }}"></script>
         <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
-        <script src="{{ asset("js_admin/datatables-simple-demo.js") }}"></script>
+        <script src="{{ asset("public/js_admin/datatables-simple-demo.js") }}"></script>
     </body>
 </html>
